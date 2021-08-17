fe39bed7b19a1e2e7b70ce1f012a707df8fcdd89
kiuolch <1473825820@qq.com>
2021-08-17 20:06:07 +0800

    second commit

--- src/main/java/com/kiuo/test/GitTest.java
+++ src/main/java/com/kiuo/test/GitTest.java
@@ -3,5 +3,6 @@ package main.java.com.kiuo.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
